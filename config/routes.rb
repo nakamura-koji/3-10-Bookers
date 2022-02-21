f8c19e0dc07379641ca460b6ee7374c123cbf161
nakamura-koji <yasumihosii2mako@gmail.com>
2022-02-21 14:24:22 +0000

    [Update] 3-10

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  get 'books_path'=>'books#index'
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
 
 end
